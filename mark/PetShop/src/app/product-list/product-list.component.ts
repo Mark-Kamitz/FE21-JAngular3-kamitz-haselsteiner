66bdb3a2405942130407e870be49f0718a726735
markus kamitz <markus_kamitz@yahoo.de>
2021-07-15 17:12:04 +0200

    pet shop added

---
+++ mark/PetShop/src/app/product-list/product-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { products } from '../products';
+import { CartService } from '../cart.service';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+
+  products = products
+  
+  constructor(private CS: CartService) { }
+  addToCart(product: any) {
+    alert("This item is added to the cart");
+    this.CS.addToCart(product);
+  }
+  ngOnInit(): void {
+  }
+
+}
---
+++ mark/PetShop/src/app/products.ts
@@ -0,0 +1,27 @@
+
+export const products = [
+    {
+      name: 'Cat',
+      price: 500,
+      description: 'Norwegian Forest Cat',
+      image: "https://cdn.pixabay.com/photo/2014/04/13/20/49/cat-323262__340.jpg"
+    },
+    {
+      name: 'Dog',
+      price: 1200,
+      description: 'Hungarian Kuvasz',
+      image: "https://cdn.pixabay.com/photo/2017/10/22/16/32/swiss-white-shepherd-2878307__340.jpg"
+    },
+    {
+      name: 'Hamster',
+      price: 60,
+      description: 'Gold Hamster',
+      image: "https://cdn.pixabay.com/photo/2015/03/26/09/41/hamster-690108__340.jpg"
+    },
+    {
+      name: 'Parrot',
+      price: 1600,
+      description: 'Big Ara',
+      image: "https://cdn.pixabay.com/photo/2018/08/12/16/59/ara-3601194__340.jpg"
+    }
+   ];
